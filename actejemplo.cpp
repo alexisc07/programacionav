fc70a068b51c53df922fe0e61d7051ce0aa63542
alexisc07 <alexisceballos471@gmail.com>
2026-08-24 16:37:58 -0600

    Create actejemplo.cpp

---
+++ actejemplo.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float salario, s_retenido, s_recibido;
+
+    cout << "Ingrese el salario mensual: ";
+    cin >> salario;
+    if(salario <= 8080) {
+        s_retenido = salario * 0.03;
+        s_recibido = salario - s_retenido;
+    }
+    else if(salario>10000 && salario <= 18000) {
+        s_retenido = salario * 0.08;
+        s_recibido = salario - s_retenido;
+    }
+    else if(salario>18000 && salario <= 20000) {
+        s_retenido = salario * 0.1;
+        s_recibido = salario - s_retenido;
+    }
+    else if(salario>20000) {
+        s_retenido = salario * 0.15;
+        s_recibido = salario - s_retenido;
+    }
+    cout << "Salario recibido: " << s_recibido << endl;
+    return 0;
+}
